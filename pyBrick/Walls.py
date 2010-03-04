f9a6c3fff62f230acc8cd39a4a836c51ef6d2f3e
doodle.m <doodle.m@a24fb47e-26c9-11df-ab0a-9f3f633ae91d>
2010-03-04 14:57:52 +0000

    minor tweaks

--- pyBrick/Walls.py
+++ pyBrick/Walls.py
@@ -1,17 +1,24 @@
-import pyglet
+# Walls class
+#
+# Creates a batch of walls either from passed file or using a default set
+#
+# Alastair Montgomery 2010
+# http://www.twitter.com/alastair_hm
+#
 
 class Walls():
     '''Define a set of walls for level'''
     walls = []
     length = 0
 
-    def __init__(self):
-        self.fileRead('defaultWalls.txt')
+    def __init__(self,filename):
+        self.fileRead(filename)
         if len(self.walls) == 0:
             '''Setup default walls if none read in'''
             self.walls.append(('##########','# # # # # ','##########','~#~#~#~#~#','#~#~#~#~#~','~#~#~#~#~#','#~#~#~#~#~'))
             self.walls.append(('##########','#~~~~~~~~#','#~######~#','#~#~~~~#~#','#~######~#','#~~~~~~~~#','##########'))
             self.walls.append(('~~~~~~~~~~','~~~~~~~~~~','##########','~~~~~~~~~~','~~~~~~~~~~','##########','~~~~~~~~~~'))
+            self.fileWrite(filename)
         self.length = len(self.walls)
 
     def fileRead(self,filename):
@@ -23,13 +30,12 @@ class Walls():
                 if line == '#Wall Start\n':
                     lines = []
                 elif line == '#Wall End\n':
-                    self.walls.append(lines)
+                    self.walls.append(lines[:7])
                 else:
                     lines.append(line.strip())
+            myFile.close()
         except IOError:
             print "Error reading from file "+filename
-        finally:
-            myFile.close()
         
     def fileWrite(self,filename):
         '''Write to a file'''
@@ -40,12 +46,9 @@ class Walls():
                 for line in wall:
                     myFile.write(line+"\n")
                 myFile.write("#Wall End\n")
+            myFile.close()
         except IOError:
             print "Error writing to file "+filename
-        finally:
-            myFile.close()
             
 if __name__ == "__main__":
-    
     w = Wall()
-    #w.fileWrite("defaultWalls.txt")
